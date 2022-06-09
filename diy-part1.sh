c2db77ffa481837845d517ce294e5ea5b87927e7
Hardy <772551228@qq.com>
2022-06-09 10:37:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,10 +19,10 @@
 
 ################
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+# echo 'https://github.com/jerrykuku/openwrt-package' >>feeds.conf.default
 
-
+# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 
